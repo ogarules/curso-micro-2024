82e77a460573b9be06bea4719cf6f8cf90792c36
Oscar <oscar@digitalchallengers.com.mx>
2024-08-10 11:59:34 -0600

    oageable

--- JPA-UnitTests/demo/src/main/java/com/example/demo/controllers/PetController.java
+++ JPA-UnitTests/demo/src/main/java/com/example/demo/controllers/PetController.java
@@ -5,11 +5,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.models.Pet;
 import com.example.demo.services.PetService;
+import com.querydsl.core.types.Predicate;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
+import org.springframework.data.web.SortDefault.SortDefaults;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,7 +47,10 @@ public class PetController {
     }
     
     @GetMapping
-    public Iterable<Pet> getAllPets() {
+    public Iterable<Pet> getAllPets(@QuerydslPredicate(root = Pet.class) Predicate predicate,
+                                    @SortDefault(sort = "name", direction = Direction.ASC)
+                                    @PageableDefault(size = 5, page = 0)
+                                    Pageable pageable) {
         log.info("Getting all pets");
         return service.getAllPets();
     }
